0a2dce4617821af644ec96e7546f5a8b10e8a4df
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-05 22:41:08 +0000

    fixed nesting of routes

--- fidirect/config/routes.rb
+++ fidirect/config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users do 
-    resources :assets do 
-      resources :recurring_payments
-    end
-    resources :investments do 
-      resources :recurring_payments 
-    end 
-    resources :loans do
-      resources :recurring_payments 
-    end 
-    resources :credit_cards do
-      resources :recurring_payments 
-    end
-    resources :bank_accounts do 
-      resources :recurring_payments
-    end
+  devise_for :users
+  resources :assets do 
+    resources :recurring_payments
+  end
+  resources :investments do 
+    resources :recurring_payments 
+  end 
+  resources :loans do
+    resources :recurring_payments 
+  end 
+  resources :credit_cards do
+    resources :recurring_payments 
+  end
+  resources :bank_accounts do 
+    resources :recurring_payments
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
