843d5ed9e8b6ef0c48d8cc7d03b52fcc1f245e3e
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-06 00:03:07 +0000

    stubbed out controller for recurring payments

--- fidirect/app/controllers/recurring_payments_controller.rb
+++ fidirect/app/controllers/recurring_payments_controller.rb
@@ -1,2 +1,35 @@
-class RecurringPaymentsController < ApplicationController
+class RecurringPaymentsController < ApplicationController 
+    def new
+    @payment = RecurringPayment.new
+  end
+
+  def create
+    @payment = RecurringPayment.create(recurring_payment_params)
+    if params[:bank_account_id]
+       @payment.bank_account = BankAccount.find[:bank_account_id]
+    elsif params[:credit_card_id] 
+         @payment.credit_card = CreditCard.find[:credit_card_id] 
+    elsif params[:investment_id] 
+         @payment.investment = Investment.find[:investment_id] 
+    elsif params[:loan_id] 
+        @payment.loan = Loan.find[:loan_id]
+    end 
+    render json: @payment
+  end
+
+  def index
+    @payments = RecurringPayment.all 
+    render json: @payments
+  end
+
+  def show
+    @payment = RecurringPayment.find(params[:id]) 
+    render json: @payment
+  end
+
+  private
+
+  def recurring_payment_params
+    params.permit(:id, :source, :status, :pay_amount, :pay_date, :duration, bank_account_name, :credit_card_provider, :investment_name, :loan_name )
+  end
 end
