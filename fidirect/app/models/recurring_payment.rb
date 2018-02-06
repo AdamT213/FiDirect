5c235dcea43c72cab3cbee8c3001ac2b120560e2
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-06 00:20:34 +0000

    stubbed out functionality for making a recurring payment recognize its account, credit card, etc

--- fidirect/app/models/recurring_payment.rb
+++ fidirect/app/models/recurring_payment.rb
@@ -2,5 +2,41 @@ class RecurringPayment < ApplicationRecord
     belongs_to :bank_account 
     belongs_to :credit_card 
     belongs_to :investment 
-    belongs_to :loan 
+    belongs_to :loan  
+    
+    def bank_account_name 
+        self.try(:bank_account).try(:name)
+    end
+
+  def bank_account_name=(name)
+    bank_account = BankAccount.find_or_create_by(name: name)
+    self.bank_account = bank_account 
+  end 
+  
+  def credit_card_provider
+    self.try(:credit_card).try(:provider)
+  end
+
+  def credit_card_provider=(provider)
+    credit_card = CreditCard.find_or_create_by(provider: provider)
+    self.credit_card = credit_card
+  end  
+  
+  def investment_name
+    self.try(:investment).try(:name)
+  end
+
+  def investment_name=(name)
+    investment = Investment.find_or_create_by(name: name)
+    self.investment = investment
+  end   
+  
+  def loan_name
+    self.try(:loan).try(:name)
+  end
+
+  def loan_name=(name)
+    loan = Loan.find_or_create_by(name: name)
+    self.loan = loan
+  end   
 end
