60364a024dddd9b4182c581ef8a78bdac2f31e4b
Adam Tropp <adt6261@gmail.com>
2018-02-06 20:24:01 +0000

    changed syntax in recurring payment model to attempt creating without all attributes

--- app/models/recurring_payment.rb
+++ app/models/recurring_payment.rb
@@ -1,42 +1,50 @@
-class RecurringPayment < ApplicationRecord 
-    belongs_to :bank_account 
-    belongs_to :credit_card 
-    belongs_to :investment 
-    belongs_to :loan  
-    
-    def bank_account_name 
+class RecurringPayment < ApplicationRecord
+    belongs_to :bank_account
+    belongs_to :credit_card
+    belongs_to :investment
+    belongs_to :loan
+
+    def bank_account_name
         self.try(:bank_account).try(:name)
     end
 
   def bank_account_name=(name)
-    bank_account = BankAccount.find_or_create_by(name: name)
-    self.bank_account = bank_account 
-  end 
-  
+    bank_account = BankAccount.find_by(name: name)
+    if bank_account
+      self.bank_account = bank_account
+    end
+  end
+
   def credit_card_provider
     self.try(:credit_card).try(:provider)
   end
 
   def credit_card_provider=(provider)
-    credit_card = CreditCard.find_or_create_by(provider: provider)
-    self.credit_card = credit_card
-  end  
-  
+    credit_card = CreditCard.find_by(provider: provider)
+    if credit_card
+      self.credit_card = credit_card
+    end
+  end
+
   def investment_name
     self.try(:investment).try(:name)
   end
 
   def investment_name=(name)
-    investment = Investment.find_or_create_by(name: name)
-    self.investment = investment
-  end   
-  
+    investment = Investment.find_by(name: name)
+    if investment
+      self.investment = investment
+    end
+  end
+
   def loan_name
     self.try(:loan).try(:name)
   end
 
   def loan_name=(name)
-    loan = Loan.find_or_create_by(name: name)
-    self.loan = loan
-  end   
+    loan = Loan.find_by(name: name)
+    if loan
+      self.loan = loan
+    end
+  end
 end
