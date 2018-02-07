029f50aeec5eb8420b6f9f9078b6f135e2f4273e
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-07 04:28:45 +0000

    added api scoping to all rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,24 @@
-Rails.application.routes.draw do
-  devise_for :users 
+Rails.application.routes.draw do 
+  scope "/api" do
+    devise_for :users 
   
-  resources :assets 
+    resources :assets 
   
-  resources :investments do 
-    resources :recurring_payments 
-  end 
+    resources :investments do 
+      resources :recurring_payments 
+    end 
   
-  resources :loans do
-    resources :recurring_payments 
-  end 
+    resources :loans do
+      resources :recurring_payments 
+    end 
  
-  resources :credit_cards do
-    resources :recurring_payments 
-  end
+    resources :credit_cards do
+      resources :recurring_payments 
+    end
   
-  resources :bank_accounts do 
-    resources :recurring_payments
+    resources :bank_accounts do 
+      resources :recurring_payments
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
