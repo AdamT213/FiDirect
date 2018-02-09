7331c59bb6e082f295d0ce27e26b280efc971384
EC2 Default User <ec2-user@ip-172-31-25-59.us-east-2.compute.internal>
2018-02-09 02:50:40 +0000

    added sessions controller and registrations controller and logic to accept json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do 
   scope "/api" do
-    devise_for :users 
+    devise_for :users, :controllers => {sessions: 'sessions', registrations: 'registrations'}
   
     resources :assets 
   
