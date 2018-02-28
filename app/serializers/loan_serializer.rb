f567f9087aed6075a557df71f7fdba41fac8fc2f
Adam Tropp <adt6261@gmail.com>
2018-02-28 18:32:46 +0000

    fixed bug in loan serializer

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,4 +1,4 @@
 class LoanSerializer < ActiveModel::Serializer
-  attributes :id, :interest_rate, :remaining_balance, :user_id 
-  has_many recurring_payments
+  attributes :id, :interest_rate, :remaining_balance, :user_id
+  has_many :recurring_payments
 end
